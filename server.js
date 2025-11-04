ac98a4b1e451ef6666b139c9e561b2a9718ac174
Ahmad Mujtaba Aman <qurayshi246@gmail.com>
2025-11-04 12:46:45 +0500

    readme.md file added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use("/writers", writerRoutes);
 //connect to Mongodb
 
 mongoose
-  .connect(mongoose.connect(process.env.MONGO_URI))
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ Error:", err));
 // crud Routes
