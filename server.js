ff337acb8c9c1f40e2506587f1e28ed34c04d00c
Ahmad Mujtaba Aman <qurayshi246@gmail.com>
2025-11-05 08:24:29 +0500

    add db.js and update .env file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const connectDB = require("./db");
 const writerRoutes = require("./routes/writerRoutes");
 require("dotenv").config();
 
@@ -11,10 +12,9 @@ app.use(express.json());
 app.use("/writers", writerRoutes);
 
 //connect to Mongodb
+connectDB();
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log("❌ Error:", err));
 // crud Routes
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("🚀 Server running on port 5000"));
